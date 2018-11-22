6faae56e1c657c84ae202eb4e7be1fbd891afc1a
sbguh <sales@batterylaptops.co.uk>
2018-11-22 18:05:12 +0800

    add commit

--- resources/views/guide/index.blade.php
+++ resources/views/guide/index.blade.php
@@ -6,22 +6,34 @@
 <div class="container">
 <div class="row">
 <div class="col-sm-3">
-<ul>
-@foreach($menu_user_guide as $menu_guide_item)
 
-<li>
-<a href="#">{{$menu_guide_item->label}}</a>
-@if($menu_guide_item->hasChildren())
-<ul>
-  @foreach($menu_guide_item->hasChildren as $sub_menu_guide_item)
-  <li><a href="{{$sub_menu_guide_item->link}}">{{$sub_menu_guide_item->label}}</a></li>
-  @endforeach
-</ul>
-@endif
-</li>
+  <div class="panel-group" id="userguide" role="tablist" aria-multiselectable="true">
+  @foreach($menu_user_guide as $menu_guide_item)
+    <div class="panel panel-default">
+     <div class="panel-heading" role="tab" id="headingOne">
+       <h4 class="panel-title">
+         <a role="button" data-toggle="collapse" data-parent="#userguide" href="#{{str_replace(' ','_',$menu_guide_item->label)}}" aria-expanded="true" aria-controls="collapseOne">
+           {{$menu_guide_item->label}}
+         </a>
+       </h4>
+     </div>
+     <div id="{{str_replace(' ','_',$menu_guide_item->label)}}" class="panel-collapse collapse">
+       <div class="panel-body">
+         @if($menu_guide_item->hasChildren())
+         <ul>
+           @foreach($menu_guide_item->hasChildren as $sub_menu_guide_item)
+           <li @if($sub_menu_guide_item->label == $guideitem->title) class="selected" @endif ><a href="{{$sub_menu_guide_item->link}}">{{$sub_menu_guide_item->label}}</a>
 
-@endforeach
-</ul>
+           </li>
+           @endforeach
+         </ul>
+         @endif
+       </div>
+     </div>
+   </div>
+   @endforeach
+  </div>
+
 </div>
 <div class="col-sm-9">
   <h1>{{$guideitem->title}}</h1>
@@ -52,6 +64,8 @@
 <script>
   $(document).ready(function () {
 
+    $("#userguide .selected").css("text-decoration",'underline');
+    $("#userguide .selected").parents(".panel-collapse").addClass('collapse in');
 
   });
 </script>
